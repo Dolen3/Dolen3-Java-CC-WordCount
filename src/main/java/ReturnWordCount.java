7da0d40d8f93af14a0b07698f305b61249c0dec4
Daniel Olen <danolen89@gmail.com>
2024-10-07 13:38:58 +0000

    Coding Challenge: Word Count Completed

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -4,7 +4,26 @@ public class ReturnWordCount {
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
-    public int count(String in){
-        return 0;
+    public int count(String in) {
+        int wordCount = 0;
+        boolean isWord = false;
+
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i) == ' ') {
+                if (isWord) {
+                    wordCount++;  // End of a word
+                    isWord = false;
+                }
+            } else {
+                isWord = true;  // Found part of a word
+            }
+        }
+
+        // If the last character(s) is part of a word, increment word count
+        if (isWord) {
+            wordCount++;
+        }
+
+        return wordCount;
     }
 }
